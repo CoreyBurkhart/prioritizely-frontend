f9a4bec41151fbd2537948326fd9fb7f1081e1f5
Corey Burkhart <hello@coreyburkhart.com>
2018-09-10 11:36:22 -0700

    implement prod server history fallback

--- server.js
+++ server.js
@@ -4,16 +4,29 @@
 const path = require('path');
 const express = require('express');
 const proxy = require('http-proxy-middleware');
+const history = require('connect-history-api-fallback');
+const chalk = require('chalk');
 const app = express();
 const PORT = 8080;
+const frontendDir = path.resolve(__dirname, './frontend');
+const proxyTarget = 'http://localhost:8081';
 
-// Serve frontend
-app.use(express.static(path.resolve(__dirname, './frontend')));
+const log = str => console.info(chalk.blue(str));
 
 // proxy requests to /api to our api
-app.use('/api', proxy({target: 'http://localhost:8081'}));
+log(`Requests to "/api" will be proxied to ${proxyTarget}`);
+app.use(/^\/api\/.+/, proxy({target: proxyTarget, verbose: true}));
+
+// Serve frontend
+log(`Serving frontend files from ${frontendDir}`);
+app.use(express.static(frontendDir));
+
+// history api fallback
+app.use(history());
+// this needs to be here to serve index.html
+app.use(express.static(frontendDir)); 
 
 // start
 app.listen(PORT, function() {
-  console.log(`Serving frontend on port ${PORT}...`);
+  log(`Serving frontend on port ${PORT}...`);
 })
